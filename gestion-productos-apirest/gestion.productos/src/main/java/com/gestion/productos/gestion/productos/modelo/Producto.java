fd5cb53bc2fb119cfb38c425e04fceeee0587cd0
Matias <desarrolloss20@gmail.com>
2022-03-01 19:00:23 -0300

    gestion de productos - API REST 01

---
+++ gestion-productos-apirest/gestion.productos/src/main/java/com/gestion/productos/gestion/productos/modelo/Producto.java
@@ -0,0 +1,48 @@
+package com.gestion.productos.gestion.productos.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nombre;
+    private String precio;
+
+    public Producto( String nombre, String precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public Producto() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+}
---
+++ gestion-productos-apirest/gestion.productos/src/main/java/com/gestion/productos/gestion/productos/repositorio/ProductoRepositorio.java
@@ -0,0 +1,8 @@
+package com.gestion.productos.gestion.productos.repositorio;
+
+
+import com.gestion.productos.gestion.productos.modelo.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductoRepositorio extends JpaRepository<Producto, Integer> {
+}
